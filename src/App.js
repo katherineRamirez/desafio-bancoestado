14ebaea0c478e72ba69913564a79860647df52ff
RoxanaCS <roxale.cares@gmail.com>
2018-04-03 20:52:13 -0300

    header y contacts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
+import Contacts from './components/Contacts';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import logo from './logo.svg';
 import './App.css';
 
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: '#F97C00',
+  },
+  appBar: {
+    height: 50,
+  },
+});
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <MuiThemeProvider muiTheme={muiTheme}>
+          <Header />
+          <Contacts/>
+          {/* <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p> */}
+        </MuiThemeProvider>
       </div>
     );
   }
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+// import Arrowbackios from 'material-ui/svg-icons/action';
+import IconButton from 'material-ui/IconButton';
+
+const Header = () => (
+  <AppBar
+    title="Logo hermoso"
+    iconElementRight={<IconButton>
+    </IconButton>}
+  />
+);
+
+export default Header;
+ 
