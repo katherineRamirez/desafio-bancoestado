da6ed1f7634e6b68f6eada68b6c9f062b5242db9
lexisR <lexisrodriguez22@gmail.com>
2018-04-03 20:11:04 -0300

    agregando funcion de eventos

--- src/components/evento/Evento.jsx
+++ src/components/evento/Evento.jsx
@@ -1,41 +1,49 @@
 import React, { Component } from 'react';
+import Invitar from './Invitar';
 
 
 class Evento extends Component {
-  /*
+
   constructor() {
     super();//Estamos heredando de otra clase para que llame al contro
     this.state = {
-  
+      contador: 1
     };
-   
-  }
-  componentWillMount() { //Se dispara una vez se renderiza
-
+    this.onButtonClick = this.onButtonClick.bind(this);
   }
 
-  */
+  onButtonClick(event) {
+    this.setState({
+      contador: 2,
+    })
+    console.log('Button Clicked !')
+  }
 
   render() {
     return (
       <div>
-        <h2 class='titleEvent'>Crea Tu evento</h2>
-        <input
-          ref={input => { this.textInput = input; }}
-          placeholder='Nombre del evento'
-          type="text" />
-        <input
-          ref={input => { this.textInput = input; }}
-          placeholder='Fecha del evento'
-          type="number" />
-        <input
-          ref={input => { this.textInput = input; }}
-          placeholder='Lugar del evento'
-          type="text" />
+        {this.state.contador === 2 ? (
+          <Invitar />
+        ) : (
+            <div>
+              <h2 class='titleEvent'>Crea Tu evento</h2>
+              <input
+                ref={input => { this.textInput = input; }}
+                placeholder='Nombre del evento'
+                type="text" />
+              <input
+                ref={input => { this.textInput = input; }}
+                placeholder='Fecha del evento'
+                type="number" />
+              <button class='btnEvent' onClick={this.onButtonClick}>
+                Crear evento
+              </button>
+            </div>
+          )}
 
-        <button class='btnEvent'>Crear evento</button>
       </div>
-    )
+
+    );
 
   }
 
