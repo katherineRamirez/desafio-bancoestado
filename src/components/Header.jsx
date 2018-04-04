fe7c80bb0fac0b717cd9b96c6e91a890a716c7e5
lexisR <lexisrodriguez22@gmail.com>
2018-04-04 09:29:53 -0300

    flujo de evento A

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
-// import Arrowbackios from 'material-ui/svg-icons/action';
-import IconButton from 'material-ui/IconButton';
-import BancoEstadoLogo from './../img/banco estado.png';
+
 import './header.css';
 
 const Header = () => (
   <AppBar
     title={<img src={BancoEstadoLogo}/>}
-    iconElementRight={<IconButton>
+    iconElementRight={<IconButton >
     </IconButton>}
+   
   />
 );
 
